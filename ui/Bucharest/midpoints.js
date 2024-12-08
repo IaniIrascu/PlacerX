92b725937a807a9725dc42cc0b21e0f0ef8a07b8
IaniIrascu <iirascu@gmail.com>
2024-12-08 08:16:20 +0200

    ui

---
+++ ui/Bucharest/midpoints.js
@@ -0,0 +1,95 @@
+var json_Bucharest_midpoints = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Potential Store",
+                "Address": "Potential Address",
+                "StoreType": "Midpoints"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    26.05091052222222,
+                    44.47948524444444
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Potential Store",
+                "Address": "Potential Address",
+                "StoreType": "Midpoints"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    26.119425359999997,
+                    44.45092255
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Potential Store",
+                "Address": "Potential Address",
+                "StoreType": "Midpoints"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    26.185685841509432,
+                    44.419716628301884
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Potential Store",
+                "Address": "Potential Address",
+                "StoreType": "Midpoints"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    26.14603964137931,
+                    44.372224031034484
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Potential Store",
+                "Address": "Potential Address",
+                "StoreType": "Midpoints"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    26.068558210526316,
+                    44.418071657894735
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Potential Store",
+                "Address": "Potential Address",
+                "StoreType": "Midpoints"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    26.01552175625,
+                    44.4279086625
+                ]
+            }
+        }
+    ]
+};
